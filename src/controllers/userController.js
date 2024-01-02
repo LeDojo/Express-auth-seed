b1911465f4560affa1b3f764db830ed521a658df
Hakim <hakim@MacBook-Pro-de-Hakim.local>
2024-01-02 15:31:27 +0100

    :sparkles:adding connexion function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,4 +12,29 @@ const store = async (req, res) => {
   }
 };
 
-module.exports = { store };
+const login = async (req, res) => {
+  email = req.body.email;
+
+  try {
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      const error = new Error("Invalid");
+      throw error;
+    }
+
+    const validPassword = await user.validPassword(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      const error = new Error("Invalid password");
+      throw error;
+    }
+    res.json({ user, message: "Vous êtes connécté" });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = { store, login };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: String,
+  email: { type: String, unique: true },
   password: String,
 });
 
@@ -11,6 +11,10 @@ userSchema.methods.encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(5);
   const hash = await bcrypt.hash(password, salt);
   return hash;
+};
+userSchema.methods.validPassword = async (candidatePassword, oldPassword) => {
+  const result = await bcrypt.compare(candidatePassword, oldPassword);
+  return result;
 };
 
 const User = mongoose.model("User", userSchema);
